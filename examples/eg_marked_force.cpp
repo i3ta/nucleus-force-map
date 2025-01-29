12641b69d463fc4b71f9ac7d669dbae430c9a8a6
aaronhung <chung62@gatech.edu>
2025-01-29 15:18:55 -0500

    WIP adding layered force calculation method

---
+++ examples/eg_marked_force.cpp
@@ -0,0 +1,62 @@
+#include "opencv2/core/matx.hpp"
+#include <image/image_reader.h>
+#include <image/image_parse.h>
+#include <iostream>
+#include <nucleus_force/nucleus_force.h>
+#include <unordered_map>
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    std::cerr << "The image path must be provided" << std::endl;
+    return 1;
+  }
+
+  // Create color mapping with 3 colors:
+  // - Empty space
+  // - Cell 
+  // - Nucleus 
+  // - Origins of force
+  std::unordered_map<cv::Vec3b, int> color_mapping;
+  color_mapping[cv::Vec3b(0, 0, 0)] = 0;
+  color_mapping[cv::Vec3b(71, 78, 147)] = 1;    // cell
+  color_mapping[cv::Vec3b(114, 186, 169)] = 2;  // nucleus
+  color_mapping[cv::Vec3b(71, 78, 147)] = 3;    // origins of force
+
+  nucleusforce::image::ColorMap cm(argv[1], color_mapping);
+
+  std::cout << "Read image into color map..." << std::endl;
+
+  std::vector<std::vector<int>> cell = nucleusforce::image::isolate_color(cm, cv::Vec3b(71, 78, 147));
+  std::vector<std::vector<int>> nucleus = nucleusforce::image::isolate_color(cm, cv::Vec3b(114, 186, 169));
+  std::vector<std::vector<int>> force_origin = nucleusforce::image::isolate_color(cm, cv::Vec3b(71, 78, 147));
+
+  std::vector<std::vector<double>> init_force(force_origin.size(), std::vector<double>(force_origin[0].size()));
+
+  // correct cell size
+  for (int r = 0; r < cell.size(); ++r) {
+    for (int c = 0; c < cell[0].size(); ++c) {
+      if (nucleus[r][c] || force_origin[r][c]) {
+        cell[r][c] = 1;
+      }
+      if (force_origin[r][c]) {
+        init_force[r][c] = 1.0; // set force from each pixel to be 1.0
+      }
+    }
+  }
+
+  std::cout << "Isolated cell and nucleus colors..." << std::endl;
+
+  std::vector<std::vector<int>> boundary = nucleusforce::find_boundary(cell, nucleus);
+  std::vector<std::vector<int>> dist = nucleusforce::find_dist(cell, nucleus);
+  std::vector<std::vector<double>> force = nucleusforce::find_nucleus_force_layer(cell, nucleus, init_force);
+
+  std::cout << "Finding force and exporting..." << std::endl;
+
+  nucleusforce::export_csv("output/boundary.csv", boundary);
+  nucleusforce::export_csv("output/dist.csv", dist);
+  nucleusforce::export_csv("output/force.csv", force);
+
+  std::cout << "Done." << std::endl;
+
+  return 0;
+}
--- src/nucleus_force/nucleus_force.cpp
+++ src/nucleus_force/nucleus_force.cpp
@@ -1,6 +1,6 @@
+#include <climits>
 #include <fstream>
 #include <nucleus_force/nucleus_force.h>
-#include <climits>
 #include <queue>
 #include <sstream>
 #include <stdexcept>
@@ -13,11 +13,13 @@ const int dx[8] = {1, 0, -1, 0, 0, 1, 0, -1};
 std::vector<std::vector<int>> find_dist(std::vector<std::vector<int>> cell,
                                         std::vector<std::vector<int>> nucleus) {
   if (cell.size() != nucleus.size() || cell[0].size() != nucleus[0].size()) {
-    throw std::invalid_argument("cell and nucleus arrays should have the same dimensions");
+    throw std::invalid_argument(
+        "cell and nucleus arrays should have the same dimensions");
   }
 
-  std::vector<std::vector<int>> dist(cell.size(),
-                                     std::vector<int>(cell[0].size(), -1)); // -1 means not reached
+  std::vector<std::vector<int>> dist(
+      cell.size(),
+      std::vector<int>(cell[0].size(), -1)); // -1 means not reached
   std::queue<std::pair<int, std::pair<int, int>>> q;
 
   for (int y = 0; y < nucleus.size(); ++y) {
@@ -39,7 +41,7 @@ std::vector<std::vector<int>> find_dist(std::vector<std::vector<int>> cell,
       int ny = y + dy[i];
       int nx = x + dx[i];
       if (ny < 0 || ny >= cell.size() || nx < 0 || nx >= cell[0].size() ||
-        dist[ny][nx] != -1 || cell[ny][nx] == 0) {
+          dist[ny][nx] != -1 || cell[ny][nx] == 0) {
         continue;
       }
       dist[ny][nx] = d + 1;
@@ -50,7 +52,9 @@ std::vector<std::vector<int>> find_dist(std::vector<std::vector<int>> cell,
   return dist;
 }
 
-std::vector<std::vector<int>> find_boundary(std::vector<std::vector<int>> cell, std::vector<std::vector<int>> nucleus) {
+std::vector<std::vector<int>>
+find_boundary(std::vector<std::vector<int>> cell,
+              std::vector<std::vector<int>> nucleus) {
   std::vector<std::vector<int>> boundary(cell.size(),
                                          std::vector<int>(cell[0].size()));
 
@@ -61,15 +65,16 @@ std::vector<std::vector<int>> find_boundary(std::vector<std::vector<int>> cell, 
         for (int i = 0; i < 8; i++) {
           int ny = y + dy[i];
           int nx = x + dx[i];
-          
-          if (ny < 0 || ny >= cell.size() || nx < 0 || nx >= cell[0].size() || 
-            cell[ny][nx] == 0 && nucleus[ny][nx] == 0) {
+
+          if (ny < 0 || ny >= cell.size() || nx < 0 || nx >= cell[0].size() ||
+              cell[ny][nx] == 0 && nucleus[ny][nx] == 0) {
             is_boundary = true;
             break;
           }
         }
 
-        if (is_boundary) boundary[y][x] = 1;
+        if (is_boundary)
+          boundary[y][x] = 1;
       }
     }
   }
@@ -77,13 +82,14 @@ std::vector<std::vector<int>> find_boundary(std::vector<std::vector<int>> cell, 
   return boundary;
 }
 
-std::vector<std::vector<double>> find_nucleus_force(std::vector<std::vector<int>> cell, 
-                                                 std::vector<std::vector<int>> nucleus) {
+std::vector<std::vector<double>>
+find_nucleus_force(std::vector<std::vector<int>> cell,
+                   std::vector<std::vector<int>> nucleus) {
   std::vector<std::vector<int>> boundary = find_boundary(cell, nucleus);
   std::vector<std::vector<double>> force(cell.size(),
                                          std::vector<double>(cell[0].size()));
   for (int y = 0; y < cell.size(); ++y) {
-    for(int x = 0; x < cell[0].size(); ++x) {
+    for (int x = 0; x < cell[0].size(); ++x) {
       force[y][x] = boundary[y][x];
     }
   }
@@ -91,16 +97,20 @@ std::vector<std::vector<double>> find_nucleus_force(std::vector<std::vector<int>
   return find_nucleus_force(cell, nucleus, force);
 }
 
-std::vector<std::vector<double>> find_nucleus_force(std::vector<std::vector<int>> cell, 
-                                                 std::vector<std::vector<int>> nucleus, 
-                                                 std::vector<std::vector<double>> force) {
+std::vector<std::vector<double>>
+find_nucleus_force(std::vector<std::vector<int>> cell,
+                   std::vector<std::vector<int>> nucleus,
+                   std::vector<std::vector<double>> force) {
   if (cell.size() != nucleus.size() || cell.size() != force.size() ||
-    cell[0].size() != nucleus[0].size() || cell[0].size() != force[0].size()) {
-    throw std::invalid_argument("Cell, nucleus, and force array dimensions must be identical.");
+      cell[0].size() != nucleus[0].size() ||
+      cell[0].size() != force[0].size()) {
+    throw std::invalid_argument(
+        "Cell, nucleus, and force array dimensions must be identical.");
   }
 
   std::vector<std::vector<int>> dist = find_dist(cell, nucleus);
-  std::vector<std::vector<double>> f(nucleus.size(), std::vector<double>(nucleus[0].size()));
+  std::vector<std::vector<double>> f(nucleus.size(),
+                                     std::vector<double>(nucleus[0].size()));
   std::priority_queue<std::pair<int, std::pair<int, int>>> q;
 
   for (int y = 0; y < force.size(); ++y) {
@@ -117,7 +127,8 @@ std::vector<std::vector<double>> find_nucleus_force(std::vector<std::vector<int>
     int x = q.top().second.second;
     q.pop();
 
-    if (f[y][x] == 0) continue;
+    if (f[y][x] == 0)
+      continue;
 
     int min_dist = INT_MAX;
     int count = 0;
@@ -159,7 +170,105 @@ std::vector<std::vector<double>> find_nucleus_force(std::vector<std::vector<int>
   return f;
 }
 
-std::vector<double> find_nucleus_centroid(std::vector<std::vector<int>> nucleus) {
+std::vector<std::vector<double>>
+find_nucleus_force_layer(std::vector<std::vector<int>> cell,
+                         std::vector<std::vector<int>> nucleus,
+                         std::vector<std::vector<double>> force) {
+  if (cell.size() != nucleus.size() || cell.size() != force.size() ||
+      cell[0].size() != nucleus[0].size() ||
+      cell[0].size() != force[0].size()) {
+    throw std::invalid_argument(
+        "Cell, nucleus, and force array dimensions must be identical.");
+  }
+
+  const int ROWS = cell.size();
+  const int COLS = cell[0].size();
+
+  auto removable = [cell, nucleus, ROWS, COLS](int r, int c) -> bool {
+    // Check if pixel can be removed
+    if (nucleus[r][c] ||
+        ((r > 0 && cell[r - 1][c]) && (c > 0 && cell[r][c - 1]) &&
+         !(r > 0 && c > 0 && cell[r - 1][c - 1])) ||
+        ((r < ROWS - 1 && cell[r + 1][c]) && (c < COLS - 1 && cell[r][c + 1]) &&
+         !(r < ROWS - 1 && c < COLS - 1 && cell[r + 1][c + 1])) ||
+        ((r > 0 && cell[r - 1][c]) && (c < COLS - 1 && cell[r][c + 1]) &&
+         !(r > 0 && c < COLS - 1 && cell[r - 1][c + 1])) ||
+        ((r < ROWS - 1 && cell[r + 1][c]) && (c > 0 && cell[r][c - 1]) &&
+         !(r < ROWS - 1 && c > 0 && cell[r + 1][c - 1])) ||
+        ((r > 0 && cell[r - 1][c]) && (r < ROWS - 1 && cell[r + 1][c]) &&
+         !(c > 0 && cell[r][c - 1]) && !(c < COLS - 1 && cell[r][c + 1])) ||
+        ((c > 0 && cell[r][c - 1]) && (c < COLS - 1 && cell[r][c + 1]) &&
+         !(r > 0 && cell[r - 1][c]) && !(r < ROWS - 1 && cell[r + 1][c]))) {
+      // Skip processing this point
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  std::vector<std::vector<double>> f(ROWS, std::vector<double>(COLS));
+  std::queue<std::pair<int, int>> q;
+
+  // find starting points for queue and resulting force
+  for (int r = 0; r < ROWS; ++r) {
+    for (int c = 0; c < COLS; ++c) {
+      if (force[r][c] > 0.0) {
+        f[r][c] = force[r][c];
+        if (removable(r, c)) {
+          q.push(std::make_pair(r, c));
+        }
+      }
+    }
+  }
+
+  // propagate force
+  while (!q.empty()) {
+    int r = q.front().first;
+    int c = q.front().second;
+    q.pop();
+
+    // set cell[r][c] = 0 to prevent propagating to itself
+    cell[r][c] = 0;
+
+    // calculate number of pixels to propagate to
+    int prop_count = 0;
+    for (int i = std::max(0, r - 1); i < std::min(ROWS, r + 1); i++) {
+      for (int j = std::max(0, c - 1); j < std::min(COLS, c + 1); j++) {
+        if (cell[i][j] || nucleus[i][j]) {
+          prop_count++;
+        }
+      }
+    }
+
+    if (prop_count == 0) {
+      continue;
+    }
+
+    // propagate force
+    double prop_val = f[r][c] / prop_count;
+    for (int i = std::max(0, r - 1); i < std::min(ROWS, r + 1); i++) {
+      for (int j = std::max(0, c - 1); j < std::min(COLS, c + 1); j++) {
+        if (cell[i][j] || nucleus[i][j]) {
+          f[i][j] += prop_val;
+        }
+      }
+    }
+
+    // continue queue
+    for (int i = std::max(0, r - 1); i < std::min(ROWS, r + 1); i++) {
+      for (int j = std::max(0, c - 1); j < std::min(COLS, c + 1); j++) {
+        if (cell[i][j] && removable(i, j)) {
+          q.push(std::make_pair(i, j));
+        }
+      }
+    }
+  }
+
+  return f;
+}
+
+std::vector<double>
+find_nucleus_centroid(std::vector<std::vector<int>> nucleus) {
   // Find centroid of nucleus
   double mx = 0;
   double my = 0;
@@ -179,10 +288,9 @@ std::vector<double> find_nucleus_centroid(std::vector<std::vector<int>> nucleus)
   return {mx, my};
 }
 
-
 std::vector<double> find_force_vector(std::vector<std::vector<int>> nucleus,
-                                    std::vector<std::vector<double>> force) {
-std::vector<double> centroid = find_nucleus_centroid(nucleus);
+                                      std::vector<std::vector<double>> force) {
+  std::vector<double> centroid = find_nucleus_centroid(nucleus);
   double mx = centroid[0];
   double my = centroid[1];
 
@@ -213,7 +321,7 @@ inline std::pair<int, std::pair<int, int>> make_coord(int y, int x, int d) {
 }
 
 void export_csv(std::string filepath, std::vector<std::vector<int>> array) {
-  std::ofstream file(filepath);  // Open file for writing
+  std::ofstream file(filepath); // Open file for writing
 
   if (!file.is_open()) {
     throw std::runtime_error("Could not open file for writing: " + filepath);
@@ -221,7 +329,7 @@ void export_csv(std::string filepath, std::vector<std::vector<int>> array) {
 
   for (size_t i = 0; i < array.size(); ++i) {
     std::ostringstream row;
-    
+
     for (size_t j = 0; j < array[i].size(); ++j) {
       row << array[i][j];
       if (j < array[i].size() - 1) {
@@ -236,7 +344,7 @@ void export_csv(std::string filepath, std::vector<std::vector<int>> array) {
 }
 
 void export_csv(std::string filepath, std::vector<std::vector<double>> array) {
-  std::ofstream file(filepath);  // Open file for writing
+  std::ofstream file(filepath); // Open file for writing
 
   if (!file.is_open()) {
     throw std::runtime_error("Could not open file for writing: " + filepath);
@@ -244,7 +352,7 @@ void export_csv(std::string filepath, std::vector<std::vector<double>> array) {
 
   for (size_t i = 0; i < array.size(); ++i) {
     std::ostringstream row;
-    
+
     for (size_t j = 0; j < array[i].size(); ++j) {
       row << array[i][j];
       if (j < array[i].size() - 1) {
